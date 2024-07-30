cf279aa3e797e3887d89fa25ff79cc015d28f932
dhanushnaruto <dhanush568kumar@gmail.com>
2024-07-30 23:56:03 +0530

    new comit

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -42,7 +42,7 @@ const ProjectsSectionContainer = styled.section`
   padding: 20px;
   background-color: ${(props) => props.theme.palette.background.default};
   color: ${(props) => props.theme.palette.text.primary};
-  height:100vh;
+  height:90vh;
 `;
 
 const Tabs = styled.div`
